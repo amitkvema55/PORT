922dbfe35ad25492ce9f52a4a1dc66c0ec18dcbc
Amit Kumar <amitkverma551@gmail.com>
2024-06-13 19:38:21 +0530

    Update Home.jsx

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -179,7 +179,6 @@ function Home()
                       
 
                 {/* <!-- get in TOUCH --> */}
-
                 <div className="text-center mt-12 flex justify-center flex-col contact" id="contact">
                     <div>
                         <h4 className="text-grey-400">Get in Touch</h4>
@@ -188,8 +187,8 @@ function Home()
                     </div>
 
                     <div className="mt-20 grid grid-cols-1 gap-y-6">
-                        <div className="bg-blue-400 w-3/4 md:w-3/5 m-auto flex flex-col items-center p-10 rounded-md hover:bg-transparent hover:border-2 hover:border-blue-400">
-                            <img src="https://i.ibb.co/59MTMQg/emial.png" alt="" width="30px" height="30px" className="rounded-full" />
+                        <div className="bg-blue-400 w-4/5 md:w-3/5 m-auto flex flex-col items-center p-10 rounded-md hover:bg-transparent hover:border-2 hover:border-blue-400">
+                            <img src="https://drive.google.com/file/d/1Uxyn9B0EHU0C7pzDn-5stlPQKOiz0ZOF/view" alt="" width="30px" height="30px" className="rounded-full" />
                             <h2 className="text-white text-xl font-semibold">Email</h2>
                             <h2 className="text-white font-bold text-xs md:text-2xl">amitkverma551@gmail.com</h2>
                         </div>
@@ -200,16 +199,23 @@ function Home()
                 <h2 className="text-white font-bold text-xs  md:text-2xl">amitkverma551@gmail.com</h2>
             </div> --> */}
 
-                        <div className="bg-blue-400 w-3/4  md:w-3/5 m-auto flex flex-col items-center p-10 rounded-md hover:bg-transparent hover:border-2 hover:border-blue-400">
-                            <img src="https://i.ibb.co/64ySHMY/call.webp" alt="" width="30px" height="30px" className="rounded-full" />
+                        <div className="bg-blue-400 w-4/5  md:w-3/5 m-auto flex flex-col items-center p-10 rounded-md hover:bg-transparent hover:border-2 hover:border-blue-400">
+                            <img src="https://drive.google.com/drive/u/0/home" alt="" width="30px" height="30px" className="rounded-full" />
                             <h2 className="text-white text-xl font-semibold">Call</h2>
-                            <h2 className="text-white font-bold text-xs md:text-2xl">9461240856</h2>
+                            <h2 className="text-white font-bold text-w md:text-2xl">9461240856</h2>
                         </div>
 
 
 
                     </div>
                 </div>
+
+
+              
+
+
+
+                  
 
                 {/* <!-- footer --> */}
 
